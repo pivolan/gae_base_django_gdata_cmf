625e6ac234627d568560fd8431fca24537747ad7
pivo <pivolan@gmail.com>
2011-12-02 22:59:56 +0400

    Доставание из google docs

--- application/main/views.py
+++ application/main/views.py
@@ -17,6 +17,11 @@ from google.appengine.api import memcache
 from google.appengine.api import mail
 from google.appengine.api import users as googleUser
 
+from library.system.BeautifulSoup import BeautifulStoneSoup
+
+import gdata.docs.data
+import gdata.docs.client
+
 import timeit
 import re
 import random
@@ -24,9 +29,11 @@ import datetime
 from datetime import timedelta
 import os
 
+class view: pass
+
 @render_to("main/index.html")
 def index(request):
-	return {}
+	return view.__dict__
 
 
 @render_to("main/offer.html")
@@ -53,3 +60,15 @@ def certification(request):
 @render_to("main/energy.html")
 def energy(request):
 	return {}
+
+def test(request):
+	client = gdata.docs.client.DocsClient(source='yourCo-yourAppName-v1')
+	client.ssl = True # Force all API requests through HTTPS
+	client.http_client.debug = True # Set to True for debugging HTTP requests
+	client.ClientLogin(settings.DOCS_EMAIL, settings.DOCS_PASS, client.source)
+
+	view.entry = client.GetFileContent('/feeds/download/documents/Export?id=1ebCRp9Q0_7bxNwtAZr4XYC2sGOdXk3ij6kEpmi-P64Y&format=html')
+	html = BeautifulStoneSoup(view.entry, convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
+	view.body = html.body.renderContents()
+	view.style = html.style.prettify()
+	return view.__dict__
